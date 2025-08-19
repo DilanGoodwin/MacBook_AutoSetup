e182913f7f4e93b1ee20f56f749162bba8ca2534
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-08-19 20:01:29 +0100

    Updated Brew Casks

--- installer.sh
+++ installer.sh
@@ -9,13 +9,14 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # HomeBrew Taps
-brew install git tmux curl gdb cmake lua openjdk python pyenv perl qmk ripgrep screen tree-sitter readline
+brew install git tmux curl gdb cmake lua openjdk python pyenv perl qmk-toolbox ripgrep screen tree-sitter readline
 
 # HomeBrew Casks
-brew install --cask firefox iterm2 1password font-ubuntu-nerd-font 1password-cli rectangle logi-options+
+brew install --cask firefox iterm2 1password font-ubuntu-nerd-font 1password-cli rectangle
 
 # Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
 # Install Nvim
 
 # Configure Nvim 
