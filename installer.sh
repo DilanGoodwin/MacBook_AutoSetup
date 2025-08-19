6bac028bc9deae4c8068de8feaa02147ce2a1280
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-08-19 19:52:36 +0100

    Updated Brew install method

--- installer.sh
+++ installer.sh
@@ -1,7 +1,22 @@
 
 # Install HomeBrew
-NONINTERACTIVE=1 /bin/bash -c \ "$(curl -fsSL \ https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c \
+   "$(curl -fsSL \
+      https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> $HOME/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # HomeBrew Taps
+brew install git tmux curl gdb cmake lua openjdk python pyenv perl qmk ripgrep screen tree-sitter readline
 
 # HomeBrew Casks
+brew install --cask firefox iterm2 1password font-ubuntu-nerd-font 1password-cli rectangle logi-options+
+
+# Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+# Install Nvim
+
+# Configure Nvim 
+
