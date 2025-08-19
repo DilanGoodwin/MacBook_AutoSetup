1a2a07ac16ef7767fc3d660a85c3cc187f340040
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-08-19 22:46:53 +0100

    Changed logging value

--- installer.sh
+++ installer.sh
@@ -4,17 +4,17 @@ declare -a Homebrew_Taps=("git" "tmux" "curl" "gdb" "cmake" "lua" "rust" "node" 
 declare -a Homebrew_Cask=("firefox" "iterm2" "1password" "1password-cli" "font-ubuntu-nerd-font" "rectangle-pro")
 
 touch $HOME/Downloads/MacBook_AutoSetup_log.txt
-log=$HOME/Downloads/MacBook_AutoSetup_log.txt
+logger="{$HOME/Downloads/MacBook_AutoSetup_log.txt}"
 
 # Xcode
 echo "Installing Xcode Tools"
-sudo xcode-select --install >> $log 
+sudo xcode-select --install >> $logger 
 
 # Install HomeBrew
 echo "Installing Homebrew"
 NONINTERACTIVE=1 /bin/bash -c \
    "$(curl -fsSL \
-      https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >> $log 
+      https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >> $logger
 
 echo >> $HOME/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
@@ -24,20 +24,20 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 for tap in "${Homebrew_Taps[@]}"
 do
   echo "Brew Installing $tap"
-  brew install $tap >> $log
+  brew install $tap >> $logger
 done
 
 for cask in "${Homebrew_Cask[@]}"
 do
   echo "Brew Installing $cask"
-  brew install --cask $cask >> $log
+  brew install --cask $cask >> $logger
 done
 
 echo "Cleaning Installation Files from Brew"
 brew cleanup
 
 # Oh-My-Zsh & Tmux Configs
-git clone https://github.com/DilanGoodwin/MacBook_AutoSetup.git >> $log
+git clone https://github.com/DilanGoodwin/MacBook_AutoSetup.git >> $logger
 
 echo "Copying zsh Config"
 rm $HOME/.zshrc
@@ -52,23 +52,23 @@ rm -rf MacBook_AutoSetup
 # Install Nvim
 echo "Install Nvim"
 mkdir -p $HOME/Documents/GitHub
-git clone https://github.com/neovim/neovim.git $HOME/Documents/GitHub/neovim >> $log
+git clone https://github.com/neovim/neovim.git $HOME/Documents/GitHub/neovim >> $logger
 
 cd $HOME/Documents/GitHub/neovim
-git checkout stable >> $log
-make CMAKE_BUILD_TYPE=Release >> $log
-sudo make install >> $log 
+git checkout stable >> $logger
+make CMAKE_BUILD_TYPE=Release >> $logger
+sudo make install >> $logger
 :
 
 echo "Adding Nvim Config"
 mkdir -p $HOME/.config/nvim
-git clone https://github.com/DilanGoodwin/nvim.config.git $HOME/.config/nvim >> $log
+git clone https://github.com/DilanGoodwin/nvim.config.git $HOME/.config/nvim >> $logger
 
 # Install Oh-My-Zsh
 echo "Installing Oh-My-Zsh"
 KEEP_ZSHRC='yes'
 OVERWRITE_CONFIRMATION='no'
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended >> $log 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended >> $logger
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
