867a3657210c2c7b5d9066d7ffda577b54243a15
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-08-19 20:26:13 +0100

    Added Install Messages

--- installer.sh
+++ installer.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+Homebrew_Taps=("git" "tmux" "curl" "gdb" "cmake" "lua" "openjdk" "python" "pyenv" "perl" "qmk-toolbox" "ripgrep" "screen" "tree-sitter" "readline" "zsh")
+Homebrew_Cask=("firefox" "iterm2" "1password" "1password-cli" "font-ubuntu-nerd-font" "rectangle")
+
+# Xcode
+sudo xcode-select --install
 
 # Install HomeBrew
 NONINTERACTIVE=1 /bin/bash -c \
@@ -8,14 +15,28 @@ echo >> $HOME/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# HomeBrew Taps
-brew install git tmux curl gdb cmake lua openjdk python pyenv perl qmk-toolbox ripgrep screen tree-sitter readline
+# HomeBrew
+for tap in Homebrew_Taps
+do
+  echo "Brew Installing $tap"
+  brew install $tap
+done
 
-# HomeBrew Casks
-brew install --cask firefox iterm2 1password font-ubuntu-nerd-font 1password-cli rectangle
+for cask in Homebrew_Cask
+do
+  echo "Brew Installing $cask"
+  brew install --cask $cask 
+done
 
-# Oh-My-Zsh
+# Oh-My-Zsh & Tmux
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
+git clone https://github.com/DilanGoodwin/MacBook_AutoSetup.git
+rm $HOME/.zshrc
+cp MacBook_AutoSetup/.zshrc $HOME/.zshrc
+
+mkdir -p $HOME/.config/tmux
+cp MacBook_AutoSetup/tmux.conf $HOME/.config/tmux/tmux.conf
 
 # Install Nvim
 
