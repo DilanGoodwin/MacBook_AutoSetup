c98bc263cf3dad5b1a5985af4e254637252a8aa8
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-08-31 13:31:12 +0100

    Force Reboot End of Script

--- installer.sh
+++ installer.sh
@@ -77,4 +77,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc >> $logger
 
-reboot
+sudo reboot
