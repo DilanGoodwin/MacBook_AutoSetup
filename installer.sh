2d2f65afc4e3286dbb434ab19232c66e49fe650a
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-08-31 13:39:11 +0100

    Corrected Git Clone For Desired Repos

--- installer.sh
+++ installer.sh
@@ -47,22 +47,23 @@ done
 echo "Cleaning Installation Files from Brew"
 brew cleanup >> $logger
 
+# Clone Desired Repos
+git clone https://github.com/DilanGoodwin/MacBook_AutoSetup.git $HOME/Documents/GitHub/MacBook_AutoSetup >> $logger
+git clone https://github.com/neovim/neovim.git $HOME/Documents/GitHub/neovim >> $logger
+git clone https://github.com/DilanGoodwin/nvim.config.git $HOME/.config/nvim >> $logger
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/omz/plugins/zsh-autosuggestions >> $logger
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/omz/plugins/zsh-syntax-highlighting >> $logger
+
 # Tmux Configs
 echo "Copying Tmux Config"
 cp $HOME/Documents/GitHub/MacBook_AutoSetup/tmux.conf $HOME/.config/tmux/tmux.conf
 
 # Install Nvim
 echo "Install Nvim"
-git clone https://github.com/neovim/neovim.git $HOME/Documents/GitHub/neovim >> $logger
-
 git checkout stable >> $logger
 make CMAKE_BUILD_TYPE=Release >> $logger
 sudo make install >> $logger
 :
-
-echo "Adding Nvim Config"
-git clone https://github.com/DilanGoodwin/nvim.config.git $HOME/.config/nvim >> $logger
-
 # Configure MacOS Settings
 chmod +x $HOME/Documents/GitHub/MacBook_AutoSetup/SettingsConfigure.sh >> $logger
 sh $HOME/Documents/GitHub/MacBook_AutoSetup/SettingsConfigure.sh >> $logger
@@ -72,9 +73,7 @@ echo "Installing Oh-My-Zsh"
 rm $HOME/.zshrc
 cp $HOME/Documents/GitHub/MacBook_AutoSetup/.zshrc $HOME/.zshrc
 
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/omz/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/omz/plugins/zsh-syntax-highlighting
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc >> $logger
 
+# Reboot For Settings to Take Affect
 sudo reboot
