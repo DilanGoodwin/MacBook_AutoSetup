a07c55ddd755eebcf257e52435b719311bf09f43
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-08-31 18:49:44 +0100

    Added iTerm2 Settings Using plutil

--- SettingsConfigure.sh
+++ SettingsConfigure.sh
@@ -38,5 +38,24 @@ plutil -replace DesktopViewSettings.IconViewSettings.iconSize -integer 56 $finde
 plutil -replace DesktopViewSettings.IconViewSettings.textSize -integer 11 $finder 
 
 # iTerm2 Settings
+term="$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+plutil -replace EnableDivisionView -integer 0 $term
+plutil -replace HapticFeedbackForEsc -integer 0 $term
+plutil -replace HideMenuBarInFullscreen -integer 1 $term
+plutil -replace HidTabNumber -integer 1 $term
+plutil -replace "Normal Font" -string "BlexMonoNF 12" $term
+plutil -replace ShowFullScreenTabBar -integer 0 $term
+plutil -replace SoundForEsc -integer 0 $term
+plutil -replace StatusBarPosition -integer 0 $term
+plutil -replace StretchTabsToFillBar -integer 0 $term
+plutil -replace TabViewType -integer 1 $term
+plutil -replace TabsHaveCloseButton -integer 0 $term
+plutil -replace VisualIndicatorForEsc -integer 0 $term
+plutil -replace WindowNumber -integer 0 $term
 
 # RectangePro Settings
+
+
+# Reboot
+# Reboot is needed for setting changes to take affect
+sudo reboot
